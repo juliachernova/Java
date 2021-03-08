43747254019d046138404c8ce0ca9ab4b66f44bf
01f <43739093+01f@users.noreply.github.com>
2021-03-08 22:39:36 +0300

    Update 002_printing_data.java

--- 002_printing_data.java
+++ 002_printing_data.java
@@ -4,5 +4,7 @@ public class Main {
       System.out.print("hey"); // просто вывод
       
       System.out.println("hey"); // вывод с переносом на новую строку
+      System.out.println(123); // вывод числа
+      System.out.println('d'); // вывод символа
     }
 }
