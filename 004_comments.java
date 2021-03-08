adee109914e33843f5a715b5572415f92d6007a9
01f <43739093+01f@users.noreply.github.com>
2021-03-08 22:57:10 +0300

    Create 004_comments.java

---
+++ 004_comments.java
@@ -0,0 +1,11 @@
+class Program {
+    public static void main(String[] args) {
+        // End of line comments
+        System.out.println("Hello, Java"); // Here can be any comment
+      
+        /*  This is an example of
+            a multi-line comment */
+      
+        
+    }
+}
