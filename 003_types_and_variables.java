657d2894c4b1c8bafbf74267854c9f66a76a5711
01f <43739093+01f@users.noreply.github.com>
2021-03-08 22:55:09 +0300

    Update 003_types_and_variables.java

--- 003_types_and_variables.java
+++ 003_types_and_variables.java
@@ -4,6 +4,20 @@ public class Main {
 
     public static void main(String[] args) {
         String name = "str";
-        int number = 6;
+        int num = 6;
+        
+        // Доступ
+        String dayOfWeek = "Monday";
+        System.out.println(dayOfWeek); // Monday
+        
+        int number = 10;
+        number = 11; // ok
+        number = "twelve"; // it does not work!
+        
+        // Альтернативные способы доступа
+        String language = "java", version = "8 or newer";
+        
+        int age; // declaration
+        age = 35; // initialization 
     }
 }
